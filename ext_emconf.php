552c337dde460d409d661381928ed0aaa12b4c4d
kasper <kasper@735d13b6-9817-0410-8766-e36946ffe9aa>
2004-03-25 07:38:10 +0000

    Initial revision
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rte/trunk@1137 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,37 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: 'rte'
+# 
+# Auto generated 22-10-2003 23:11
+# 
+# Manual updates:
+# Only the data in the array - anything else is removed by next write
+########################################################################
+
+$EM_CONF[$_EXTKEY] = Array (
+	'title' => 'Rich Text Editor',
+	'description' => 'Rich Text Editor based on MSIE Active X. This is the one that has been around all the time. Now it\'s just an extension...',
+	'category' => 'be',
+	'shy' => 1,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => 'app',
+	'state' => 'stable',
+	'internal' => 1,
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => 'G',
+	'author' => 'Kasper Skårhøj',
+	'author_email' => 'kasper@typo3.com',
+	'author_company' => 'Curby Soft Multimedia',
+	'private' => 0,
+	'download_password' => '',
+	'version' => '0.0.8',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:21:{s:12:"doc/TODO.txt";s:4:"ad06";s:14:"doc/manual.sxw";s:4:"d4db";s:13:"app/clear.gif";s:4:"cc11";s:12:"app/conf.php";s:4:"e95b";s:17:"app/locallang.php";s:4:"96ff";s:34:"app/locallang_rte_select_image.php";s:4:"a59a";s:26:"app/locallang_rte_user.php";s:4:"380f";s:10:"app/rte.js";s:4:"ab39";s:11:"app/rte.php";s:4:"5322";s:16:"app/rte_bold.gif";s:4:"802d";s:19:"app/rte_cleaner.php";s:4:"0888";s:16:"app/rte_copy.gif";s:4:"11a1";s:15:"app/rte_cut.gif";s:4:"2950";s:15:"app/rte_div.gif";s:4:"d998";s:19:"app/rte_italics.gif";s:4:"1231";s:17:"app/rte_paste.gif";s:4:"083c";s:15:"app/rte_res.php";s:4:"a790";s:24:"app/rte_select_image.php";s:4:"53c3";s:16:"app/rte_tbEN.gif";s:4:"321f";s:17:"app/rte_under.gif";s:4:"b2f0";s:16:"app/rte_user.php";s:4:"388e";}',
+);
+
+?>
