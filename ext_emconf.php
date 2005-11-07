2261d6341de7c47d5c5416db25e2b946e0bd4b1d
kasper <kasper@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-11-07 12:23:00 +0000

    *** empty log message ***
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rte/trunk@1151 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,9 +2,9 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "rte"
-#
-# Auto generated 26-04-2004 15:20
-#
+# 
+# Auto generated 07-10-2005 15:32
+# 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
 ########################################################################
@@ -35,8 +35,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '0.0.10',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:31:{s:21:"class.tx_rte_base.php";s:4:"8161";s:17:"ext_localconf.php";s:4:"6fa5";s:11:"CVS/Entries";s:4:"e4d0";s:14:"CVS/Repository";s:4:"28ae";s:8:"CVS/Root";s:4:"bbeb";s:13:"app/clear.gif";s:4:"cc11";s:12:"app/conf.php";s:4:"e95b";s:17:"app/locallang.php";s:4:"7598";s:34:"app/locallang_rte_select_image.php";s:4:"5227";s:26:"app/locallang_rte_user.php";s:4:"0886";s:10:"app/rte.js";s:4:"ab39";s:11:"app/rte.php";s:4:"03a7";s:16:"app/rte_bold.gif";s:4:"802d";s:19:"app/rte_cleaner.php";s:4:"fba8";s:16:"app/rte_copy.gif";s:4:"11a1";s:15:"app/rte_cut.gif";s:4:"2950";s:15:"app/rte_div.gif";s:4:"d998";s:19:"app/rte_italics.gif";s:4:"1231";s:17:"app/rte_paste.gif";s:4:"083c";s:24:"app/rte_select_image.php";s:4:"ce02";s:16:"app/rte_tbEN.gif";s:4:"321f";s:17:"app/rte_under.gif";s:4:"b2f0";s:16:"app/rte_user.php";s:4:"acad";s:15:"app/CVS/Entries";s:4:"7c33";s:18:"app/CVS/Repository";s:4:"2a38";s:12:"app/CVS/Root";s:4:"bbeb";s:12:"doc/TODO.txt";s:4:"ad06";s:14:"doc/manual.sxw";s:4:"7ec5";s:15:"doc/CVS/Entries";s:4:"10c5";s:18:"doc/CVS/Repository";s:4:"0d63";s:12:"doc/CVS/Root";s:4:"bbeb";}',
+	'version' => '0.0.11',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:33:{s:21:"class.tx_rte_base.php";s:4:"718b";s:17:"ext_localconf.php";s:4:"6fa5";s:11:"CVS/Entries";s:4:"bdc1";s:15:"CVS/Entries.Log";s:4:"6be0";s:14:"CVS/Repository";s:4:"28ae";s:8:"CVS/Root";s:4:"bbeb";s:17:"app/.#rte.php.1.2";s:4:"4384";s:13:"app/clear.gif";s:4:"cc11";s:12:"app/conf.php";s:4:"e95b";s:17:"app/locallang.php";s:4:"7598";s:34:"app/locallang_rte_select_image.php";s:4:"5227";s:26:"app/locallang_rte_user.php";s:4:"0886";s:10:"app/rte.js";s:4:"d803";s:11:"app/rte.php";s:4:"2dd1";s:16:"app/rte_bold.gif";s:4:"802d";s:19:"app/rte_cleaner.php";s:4:"8468";s:16:"app/rte_copy.gif";s:4:"11a1";s:15:"app/rte_cut.gif";s:4:"2950";s:15:"app/rte_div.gif";s:4:"d998";s:19:"app/rte_italics.gif";s:4:"1231";s:17:"app/rte_paste.gif";s:4:"083c";s:24:"app/rte_select_image.php";s:4:"9c1d";s:16:"app/rte_tbEN.gif";s:4:"321f";s:17:"app/rte_under.gif";s:4:"b2f0";s:16:"app/rte_user.php";s:4:"c993";s:15:"app/CVS/Entries";s:4:"8c7c";s:18:"app/CVS/Repository";s:4:"2a38";s:12:"app/CVS/Root";s:4:"bbeb";s:12:"doc/TODO.txt";s:4:"2851";s:14:"doc/manual.sxw";s:4:"7ec5";s:15:"doc/CVS/Entries";s:4:"943e";s:18:"doc/CVS/Repository";s:4:"0d63";s:12:"doc/CVS/Root";s:4:"bbeb";}',
 );
 
 ?>
